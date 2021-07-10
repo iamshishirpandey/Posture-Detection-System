6ce54ad78a5cc43c070dbbe5955b7ad13613fea4
iamshishirpandey <iamshishirpandey@gmail.com>
2021-07-10 11:44:42 +0545

    Instantaniate Camera Function validation before app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+/// The list of camera types (mainly including: front and back)
+List<CameraDescription> cameras = [];
 void main() {
+  try {
+    // To load the cameras before the app is initialized
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error: ${e.code}\nError Message: ${e.description}');
+  }
   runApp(MyApp());
 }
 
@@ -9,6 +20,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp();
   }
 }
---
+++ lib/screens/login/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Loginscreen extends StatefulWidget {
+  const Loginscreen({Key key}) : super(key: key);
+
+  @override
+  _LoginscreenState createState() => _LoginscreenState();
+}
+
+class _LoginscreenState extends State<Loginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
